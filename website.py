4fc69551d24b82b9cce6dade86889c38eba34b8f
Mel Chua <mel@melchua.com>
2017-01-31 21:05:24 -0500

    removed prepending code, now in crawler.py

--- website.py
+++ website.py
@@ -9,11 +9,5 @@ class Website:
 		self.resultListing = resultListing
 		self.resultUrl = resultUrl
 		self.absoluteUrl=absoluteUrl
-
-		#prepend a slash to relative URLs if not already done
-		if absoluteUrl == "FALSE" and resultUrl[0]!='/':
-			self.resultUrl = '/' + self.resultUrl
-
-		print("resultUrl is", self.resultUrl)	
 		self.pageTitle = pageTitle
 		self.pageBody = pageBody
