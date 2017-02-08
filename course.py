746b032b2c84852c57babf96f6da97309b3fbff8
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-02-07 20:36:52 -0500

    Update course object to include college

--- course.py
+++ course.py
@@ -1,11 +1,12 @@
 class Course:
     '''common base class for all courses'''
 
-    def __init__(self, title, subject, description):
+    def __init__(self, title, subject, description, college):
        self.title = title
        self.subject = subject
        self.description = description
+       self.college = college
 
 
     def __str__(self):
-        return(self.title + ' in ' + self.subject + '\n' + self.description)
+        return(self.title + ' in ' + self.subject + ' at ' + self.college + '\n' + self.description)
